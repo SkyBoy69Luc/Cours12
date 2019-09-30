459f4ef8a58929d6d04e3ae7e2dc6b79b907b696
SkyBoy <luc.plante.guitare.skyboy69@gmail.com>
2019-09-30 15:42:39 -0400

    Version final

---
+++ Documents_etudiants_Cours12/Assets/Script/EndGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour {
+
+    public AudioSource son;
+	// Use this for initialization
+	void Start () {
+        son = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        son.Play();
+
+    }
+}
